abc60e50834dc30431b5e94cdb7e2ee57502c12b
kailashgp <51281708+kailashgp@users.noreply.github.com>
2021-12-29 07:54:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+@app.route("/")
+def hello():
+    return "Hello, World!"
